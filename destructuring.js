84fa8a4064eb86bb010f1a549c60b66f9120c9e2
bd-dev-ismail <ismailratul7@gmail.com>
2022-08-26 15:59:19 +0600

    destructuring

---
+++ destructuring.js
@@ -0,0 +1,13 @@
+// ৮. সিম্পল একটা জাভাস্ক্রিপ্ট অবজেক্ট এর কোন একটা প্রোপার্টিকে ভেরিয়েবল হিসেবে ডিক্লেয়ার করার জন্য destructuring ইউজ করো। 
+const obj = {name: 'Md Ismail Hossen', roll: '238', class: 'javascript', institue: 'Programming Hero'};
+const {roll} = obj;
+// console.log(roll);
+
+// ৯. [চ্যালেঞ্জিং] array এর destructuring করবে আর সেটা করার জন্য তুমি এরে এর থার্ড পজিশন এর উপাদান কে destructuring করে 'three' নামক একটা ভেরিয়েবল এ রাখবে। 
+const arr = [22, 44, 66, 88,];
+const [one, two, three] = arr;
+// console.log(three);
+
+// ১০. তিনটা প্যারামিটার ওয়ালা একটা ফাংশন লিখবে। যেই ফাংশনের কাজ হবে তিনটা প্যারামিটার নিয়ে সেই তিনটা প্যারামিটার এর যোগ করে যোগফল রিটার্ন করবে। আর থার্ড প্যারামিটার এর একটা ডিফল্ট ভ্যালু থাকবে। সেটা হবে ৭।
+const threeParameter = (num1, num2, num3 = 7) => num1 + num2 + num3;
+console.log(threeParameter(2,2)); 
